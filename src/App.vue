8c0e4a4c7477de892db03caaad9169bb9591c195
medaminebenchouikha <med.amine.ben.chouikha@gmail.com>
2024-05-29 22:01:54 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li class="nav-item">
+          <img src="./assets/build-a-bot-logo.png" alt="logo" class="logo">
+          Build-a-Bot
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <!-- <HomePage msg="Welcome to Your Vue.js App"/> -->
+    <ProductSearch/>
+  </main>
+</template>
+
+<script>
+// import RobotBuilder from './build/RobotBuilder.vue';
+// import HomePage from './home/HomePage.vue';
+import ProductSearch from './search/ProductSearch.vue';
+
+export default {
+  name: 'App',
+  components: {
+    // HomePage,
+    ProductSearch,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+
+body {
+  background: linear-gradient(to bottom, #555, #999);
+  background-attachment: fixed
+}
+
+main {
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1200px;
+  min-height: 200px;
+}
+
+header {
+  background-color: #999;
+  width: 1260px;
+  margin: 0 auto;
+}
+
+ul {
+  padding: 3px;
+  display: flex;
+}
+
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #bbb;
+}
+
+.logo {
+  vertical-align: middle;
+  height: 30px;
+}
+</style>
---
+++ src/home/HomePage.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png" alt="Robot" aria-hidden="true"/>
+      <div class="get-started">
+        <a href="">Get started</a> Vuilding your first robot!
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home {
+  text-align: center;
+}
+
+.robot {
+  height: 400px;
+}
+
+.get-started {
+  padding-top: 20px;
+  font-size: 22px;
+}
+</style>
